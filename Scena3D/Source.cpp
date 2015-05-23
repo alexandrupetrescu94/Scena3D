da92431636e374a36b15726767fa29fabf12ef79
AlexP <alexandrupetrescu94@yahoo.com>
2015-05-23 11:28:17 +0300

    Keyboard works - But Add More Movements
    Add new movements

--- Scena3D/Source.cpp
+++ Scena3D/Source.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 using namespace std;
 
+const int TEXDIM = 256;
+
 //Observer Coords
 GLfloat x0 = 50.0, y0 = 450.0, z0 = 300.0;
 GLfloat xref = 0.0, yref = 300.0, zref = 0.0;
@@ -11,11 +13,8 @@ GLfloat Vx = 0.0, Vy = 1.0, Vz = 0.0;
 //Projection Coords
 GLfloat dnear = 1.0, dfar = 40.0;
 
-
-GLfloat xwMin = -30.0, ywMin = -30.0, xwMax = 30.0, ywMax = 30.0;
 float angle = 0.0; 		// angle of rotation for the camera direction
-float lx = 0.0f,lz = -1.0f; // actual vector representing the camera's direction
-
+float lx = -1.0f,lz = -1.0f; // actual vector representing the camera's direction
 
 GLfloat *tex;
 GLfloat * make_texture(int maxs, int maxt) {
@@ -31,7 +30,6 @@ GLfloat * make_texture(int maxs, int maxt) {
 	return texture;
 }
 
-
 GLUquadricObj *sphereLight;
 GLfloat lightpos[] = { -150.f, -150.f, 50.f, 1.f };
 enum {
@@ -40,8 +38,6 @@ enum {
 enum {
 	SPHERE = 1, LIGHT, LEFTWALL, FLOOR
 };
-
-
 
 void build_map(){
 	/* Left Block */
@@ -161,17 +157,24 @@ void build_house(){
 	glEnd();
 }
 
-void renderScene(void)
-{
+void renderScene(void){
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	
+	// Reset transformations
+	glLoadIdentity();
+	
+	// Add the new transformation
+	glFrustum(-100., 100., -100., 100., 200., 1100.); // raportat la observator
+	gluLookAt(x0, y0, z0, xref, yref, zref, Vx, Vy, Vz); // reset camera
+	cout << "zref= " << zref << endl;
 
 	/* material properties for objects in scene */
 	static GLfloat wall_mat[] = { 1.f, 1.f, 1.f, 1.f };
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, wall_mat);
-
 	build_house();
 	build_map();
 
+	//add sphereLight
 	glPushMatrix();
 	glTranslatef(lightpos[X], lightpos[Y], lightpos[Z]);
 	glDisable(GL_LIGHTING);
@@ -183,18 +186,14 @@ void renderScene(void)
 	glutSwapBuffers();
 }
 
-void key(unsigned char key, int x, int y)
-{
+void key(unsigned char key, int x, int y){
 	if (key == '\033')
 		exit(0);
 }
 
-const int TEXDIM = 256;
-
 void processSpecialKeys(int key, int xx, int yy) {
 
-	float fraction = 0.1f;
-	cout << 'a';
+	float fraction = 1.0f;
 	switch (key) {
 		case GLUT_KEY_UP :
 			xref += lx * fraction;
@@ -207,16 +206,7 @@ void processSpecialKeys(int key, int xx, int yy) {
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	glutInit(&argc, argv);
-	glutInitWindowSize(800,800);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL | GLUT_DOUBLE);
-	(void)glutCreateWindow("Scena3D");
-	glutDisplayFunc(renderScene);
-	glutKeyboardFunc(key);
-	glutSpecialFunc(processSpecialKeys);
-
+void initialize(){
 	/* draw a perspective scene */
 	glMatrixMode(GL_PROJECTION);
 	glFrustum(-100., 100., -100., 100., 200., 1100.); // raportat la observator
@@ -238,8 +228,35 @@ int main(int argc, char *argv[])
 	tex = make_texture(TEXDIM, TEXDIM);
 	glTexImage2D(GL_TEXTURE_2D, 0, 1, TEXDIM, TEXDIM, 0, GL_RED, GL_FLOAT, tex);
 	free(tex);
+}
 
+int main(int argc, char *argv[]){
+	glutInit(&argc, argv);
+	glutInitWindowSize(800,800);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL | GLUT_DOUBLE);
+	glutCreateWindow("Scena3D");
+	glutDisplayFunc(renderScene);
+	glutIdleFunc(renderScene);
+	glutKeyboardFunc(key);
+	glutSpecialFunc(processSpecialKeys);
+	//glutReshapeFunc(reshape); Not Working (Optional)
+	initialize();
+	glEnable(GL_DEPTH_TEST);
 	glutMainLoop();
-
 	return 0;
 }
+
+
+/*
+Functions TODO:
+
+void reshape(GLsizei w, GLsizei h) {
+  glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glFrustum(-100., 100., -100., 100., 200., 1100.); // raportat la observator
+  glMatrixMode(GL_MODELVIEW);
+
+}
+
+*/
